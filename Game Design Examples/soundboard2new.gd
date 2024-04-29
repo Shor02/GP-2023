9799b24b01bdc805f4ac5488a0ee28cebca1c394
Shor02 <shor02@outlook.com>
2024-04-29 22:12:03 +0100

    Update soundboard2new.gd

--- Game Design Examples/soundboard2new.gd
+++ Game Design Examples/soundboard2new.gd
@@ -4,6 +4,16 @@ extends Node2D
 var sound1 = preload("res://sounds/sfx_deathscream_human1.wav")
 var sound2 = preload("res://sounds/sfx_deathscream_human3.wav")
 var sound3 = preload("res://sounds/sfx_deathscream_human12.wav")
+var samples:Array
+var players:Array
+var pads:Dictionary
+@export var path_str = "res://samples" 
+@export var pad_scene:PackedScene
+@export var note_scene:PackedScene
+
+@export var num_pads = 8
+
+var num_players = 10
 
 # Signal to play sound
 signal play_sound
@@ -31,3 +41,29 @@ func _on_button_pressed(audio_file):
 
 func _on_play_sound():
 	pass # Replace with function body.
+	
+func load_samples():
+	var dir = DirAccess.open(path_str)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		
+		# From https://forum.godotengine.org/t/loading-an-ogg-or-wav-file-from-res-sfx-in-gdscript/28243/2
+		while file_name != "":
+			if dir.current_is_dir():
+				print("Found directory: " + file_name)
+			if file_name.ends_with('.wav.import'):			
+				file_name = file_name.left(len(file_name) - len('.import'))
+				# var asp = AudioStreamPlayer.new()
+				# asp.set_stream(load(SOUND_DIR + '/' + filename))
+				# add_child(asp)
+				# var arr = file_name.split('/')
+				# var name = arr[arr.size()-1].split('.')[0]
+				# samples[name] = asp
+			
+				var stream = load(path_str + "/" + file_name)
+				stream.resource_name = file_name
+				samples.push_back(stream)
+				# $AudioStreamPlayer.play()
+				# break
+			file_name = dir.get_next()	
